2b50a7aedb96729a4d8643f498637874d8d78726
Jon Pitans <jonpitans@gmail.com>
2022-02-18 15:45:03 +0000

    sort of working

--- weather.js
+++ weather.js
@@ -7,26 +7,26 @@ navigator.geolocation.getCurrentPosition(success, failure, options);
 
 function success(position) {
  
-    //api call for open weather
-    console.log(position);
-    const Loc = document.getElementById("location");
-    Loc.textContent = `Your current location is ${position.coords.latitude} latitude and ${position.coords.longitude}
-    longitude.`;
-    document.getElementById("map").innerHTML = `<div class="mapouter"><div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=${position.coords.latitude},${position.coords.longitude}&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://123movies-to.org"></a><br><style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}</style><a href="https://www.embedgooglemap.net"></a><style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style></div></div>`;
-    fetch (`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=1826f92082fe718a7e0692c911e03e1d
-    `).then(function(response) {
-        console.log(response);
-    }
     
-    );
+    document.getElementById("map").innerHTML = `<div class="mapouter"><div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=${position.coords.latitude},${position.coords.longitude}&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://123movies-to.org"></a><br><style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}</style><a href="https://www.embedgooglemap.net"></a><style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style></div></div>`;
+  
+    let req = new XMLHttpRequest();
+    req.open("GET", `http://api.openweathermap.org/data/2.5/weather?lat=${
+        position.coords.latitude
+    }&lon=${
+        position.coords.longitude
+    }&appid=1826f92082fe718a7e0692c911e03e1d`);
 
-    fetch (`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=1826f92082fe718a7e0692c911e03e1d`);
-
+    req.responseType = "json";
+    req.send(null);
+    req.addEventListener("load", e => {
+    console.log(req.response);
+    document.getElementById("location").textContent = `Your current location is ${req.response.name}, ${position.coords.latitude} latitude and ${position.coords.longitude}
+    longitude.`;
+}   
+)
 }
 
 function failure() {
     console.log("sorry, you didn't want to share")
 }
-
-// navigator.geolocation.getCurrentPosition(success, failure);
-
